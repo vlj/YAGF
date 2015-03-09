3120ac8095a1144aaa7516bb01b41beead62968b
Vincent Lejeune <vljn@ovi.com>
2015-03-09 18:40:25 +0100

    Add a glyph quad and proper shader

--- YAGF/examples/text.cpp
+++ YAGF/examples/text.cpp
@@ -16,31 +16,47 @@ extern "C" {
 #include FT_FREETYPE_H
 }
 
-GLuint TextureA;
+GLuint Texture[256];
 
 GLuint BilinearSampler;
 
 FT_Library Ft;
 FT_Face Face;
 
+GLuint GlyphVAO;
+GLuint GlyphVBO;
+
+static const char *glyphvs =
+"#version 330\n"
+"uniform vec2 GlyphCenter;\n"
+"uniform vec2 GlyphScaling;\n"
+"layout(location = 0) in vec2 Position;\n"
+"layout(location = 1) in vec2 Texcoord;\n"
+"out vec2 uv;"
+"void main()\n"
+"{\n"
+"   uv = Texcoord;\n"
+"   gl_Position = vec4(GlyphCenter + GlyphScaling * Position, 0., 1.);\n"
+"}\n";
+
 static const char *passthrougfs =
 "#version 330\n"
 "uniform sampler2D tex;\n"
+"in vec2 uv;"
 "out vec4 FragColor;\n"
 "void main() {\n"
-"vec2 uv = gl_FragCoord.xy / vec2(1024., -1024);\n"
 "FragColor = vec4(1., 1., 1., texture(tex,uv).r);\n"
 "}\n";
 
-class FullScreenPassthrough : public ShaderHelperSingleton<FullScreenPassthrough>, public TextureRead<Texture2D>
+class GlyphRendering : public ShaderHelperSingleton<GlyphRendering, irr::core::vector2df, irr::core::vector2df>, public TextureRead<Texture2D>
 {
 public:
-    FullScreenPassthrough()
+    GlyphRendering()
     {
         Program = ProgramShaderLoading::LoadProgram(
-            GL_VERTEX_SHADER, screenquadshader,
+            GL_VERTEX_SHADER, glyphvs,
             GL_FRAGMENT_SHADER, passthrougfs);
-        AssignUniforms();
+        AssignUniforms("GlyphCenter", "GlyphScaling");
 
         AssignSamplerNames(Program, 0, "tex");
     }
@@ -55,27 +71,49 @@ void init()
         printf("Can't init Arial\n");
     FT_Set_Pixel_Sizes(Face, 0, 48);
 
-    if (FT_Load_Char(Face, 'B', FT_LOAD_RENDER)) {
-        printf("Could not load character 'X'\n");
+    glGenVertexArrays(1, &GlyphVAO);
+    glBindVertexArray(GlyphVAO);
+    glGenBuffers(1, &GlyphVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, GlyphVBO);
+    const float quad_vertex[] = {
+        -1., -1., 0., 0., // UpperLeft
+        -1., 1., 0., 1., // LowerLeft
+        1., -1., 1., 0., // UpperRight
+        1., 1., 1., 1., // LowerRight
+    };
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (GLvoid *)(2 * sizeof(float)));
+    glBufferData(GL_ARRAY_BUFFER, 16 * sizeof(float), quad_vertex, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+
+    glGenTextures(128, Texture);
+    BilinearSampler = SamplerHelper::createBilinearSampler();
+
+    for (int i = 0; i < 128; i++)
+    {
+        if (FT_Load_Char(Face, (char)i, FT_LOAD_RENDER))
+            printf("Could not load character %c\n", i);
+
+        glBindTexture(GL_TEXTURE_2D, Texture[i]);
+        glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, Face->glyph->bitmap.width, Face->glyph->bitmap.rows, 0, GL_RED, GL_UNSIGNED_BYTE, Face->glyph->bitmap.buffer);
     }
 
-    glGenTextures(1, &TextureA);
-    glBindTexture(GL_TEXTURE_2D, TextureA);
-    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, Face->glyph->bitmap.width, Face->glyph->bitmap.rows, 0, GL_RED, GL_UNSIGNED_BYTE, Face->glyph->bitmap.buffer);
-    glBindTexture(GL_TEXTURE_2D, 0);
-
-    BilinearSampler = SamplerHelper::createBilinearSampler();
 }
 
 void clean()
 {
-    glDeleteTextures(1, &TextureA);
+    glDeleteTextures(128, Texture);
 }
 
 void draw()
 {
+    const char *string = "test";
+
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
     glViewport(0, 0, 1024, 1024);
     glClearColor(0., 0., 0., 0.);
@@ -84,8 +122,12 @@ void draw()
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glBlendEquation(GL_FUNC_ADD);
-    FullScreenPassthrough::getInstance()->SetTextureUnits(TextureA, BilinearSampler);
-    DrawFullScreenEffect<FullScreenPassthrough>();
+
+    glBindVertexArray(GlyphVAO);
+    glUseProgram(GlyphRendering::getInstance()->Program);
+    GlyphRendering::getInstance()->SetTextureUnits(Texture['X'], BilinearSampler);
+    GlyphRendering::getInstance()->setUniforms(irr::core::vector2df(0., 0.), irr::core::vector2df(.5, .5));
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 }
 
 int main()
