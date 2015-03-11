847a36fdcf10a973d9a650b81737cf4063327b6f
Vincent Lejeune <vljn@ovi.com>
2015-03-11 22:25:05 +0100

    Some more cleaning

--- include/Loaders/B3D.h
+++ include/Loaders/B3D.h
@@ -22,54 +22,13 @@
 
 namespace irr
 {
-    namespace io
-    {
-        class IReadFile;
-    } // end namespace io
-    namespace scene
-    {
-        class IAnimatedMesh;
-
-        //! Class which is able to load an animated mesh from a file.
-        /** If you want Irrlicht be able to load meshes of
-        currently unsupported file formats (e.g. .cob), then implement
-        this and add your new Meshloader with
-        ISceneManager::addExternalMeshLoader() to the engine. */
-        class IMeshLoader
-        {
-        public:
-
-            //! Destructor
-            virtual ~IMeshLoader() {}
-
-            //! Returns true if the file might be loaded by this class.
-            /** This decision should be based on the file extension (e.g. ".cob")
-            only.
-            \param filename Name of the file to test.
-            \return True if the file might be loaded by this class. */
-//            virtual bool isALoadableFileExtension(const io::path& filename) const = 0;
-
-            //! Creates/loads an animated mesh from the file.
-            /** \param file File handler to load the file from.
-            \return Pointer to the created mesh. Returns 0 if loading failed.
-            If you no longer need the mesh, you should call IAnimatedMesh::drop().
-            See IReferenceCounted::drop() for more information. */
-            virtual std::vector<SMeshBufferLightMap> createMesh(io::IReadFile* file) = 0;
-        };
-
-
-    } // end namespace scene
-} // end namespace irr
-
-
-
-
-namespace irr
-{
+  namespace io
+  {
+    class IReadFile;
+  } // end namespace io
 
     namespace scene
     {
-
         class CSkinnedMesh{
         public:
             //! Joints
@@ -129,7 +88,7 @@ namespace irr
         };
 
         //! Meshloader for B3D format
-        class CB3DMeshFileLoader : public IMeshLoader
+        class CB3DMeshFileLoader
         {
         public:
 
@@ -153,7 +112,7 @@ namespace irr
             //! \return Pointer to the created mesh. Returns 0 if loading failed.
             //! If you no longer need the mesh, you should call IAnimatedMesh::drop().
             //! See IReferenceCounted::drop() for more information.
-            virtual std::vector<SMeshBufferLightMap> createMesh(io::IReadFile* file) override
+            std::vector<SMeshBufferLightMap> createMesh(io::IReadFile* file)
             {
                 if (!file)
                     return AnimatedMesh;
@@ -176,6 +135,8 @@ namespace irr
 
                 return AnimatedMesh;
             }
+
+            ~CB3DMeshFileLoader() {};
 
         private:
 
