6c7e4f0a5306410cc55c290db5caed9193b31f59
vlj <vljn.ovi@gmail.com>
2015-03-31 18:13:56 +0200

    meshdx12: Start a stub example for dx12

---
+++ examples/meshdx12.cpp
@@ -0,0 +1,90 @@
+#include <windows.h>
+#include <windowsx.h>
+#include <d3d12.h>
+#include <dxgi1_4.h>
+#include <d3dcompiler.h>
+#include <wrl/client.h>
+using namespace Microsoft::WRL; // For ComPtr
+
+// the WindowProc function prototype
+LRESULT CALLBACK WindowProc(HWND hWnd,
+	UINT message,
+	WPARAM wParam,
+	LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nCmdShow)
+{
+	// the handle for the window, filled by a function
+	HWND hWnd;
+	// this struct holds information for the window class
+	WNDCLASSEX wc;
+
+	// clear out the window class for use
+	ZeroMemory(&wc, sizeof(WNDCLASSEX));
+
+	// fill in the struct with the needed information
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.lpszClassName = "WindowClass1";
+
+	// register the window class
+	RegisterClassEx(&wc);
+
+	// create the window and use the result as the handle
+	hWnd = CreateWindowEx(NULL,
+		"WindowClass1",    // name of the window class
+		"Our First Windowed Program",   // title of the window
+		WS_OVERLAPPEDWINDOW,    // window style
+		300,    // x-position of the window
+		300,    // y-position of the window
+		500,    // width of the window
+		400,    // height of the window
+		NULL,    // we have no parent window, NULL
+		NULL,    // we aren't using menus, NULL
+		hInstance,    // application handle
+		NULL);    // used with multiple windows, NULL
+
+				  // display the window on the screen
+	ShowWindow(hWnd, nCmdShow);
+
+	// this struct holds Windows event messages
+	MSG msg;
+
+	// wait for the next message in the queue, store the result in 'msg'
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		// translate keystroke messages into the right format
+		TranslateMessage(&msg);
+
+		// send the message to the WindowProc function
+		DispatchMessage(&msg);
+	}
+	// return this part of the WM_QUIT message to Windows
+	return msg.wParam;
+}
+
+// this is the main message handler for the program
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	// sort through and find what code to run for the message given
+	switch (message)
+	{
+		// this message is read when the window is closed
+	case WM_DESTROY:
+	{
+		// close the application entirely
+		PostQuitMessage(0);
+		return 0;
+	} break;
+	}
+
+	// Handle any messages the switch statement didn't
+	return DefWindowProc(hWnd, message, wParam, lParam);
+}
