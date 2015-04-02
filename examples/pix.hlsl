468b2779d28841b709d25cec29248fefd79eaabb
vlj <vljn.ovi@gmail.com>
2015-04-02 18:52:44 +0200

    meshdx12: Make pixel shader able to read texture/sample

--- examples/pix.hlsl
+++ examples/pix.hlsl
@@ -1,14 +1,24 @@
 #define MyRS1 "RootFlags( ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT), " \
-                         "DescriptorTable(CBV(b0)) "
+                         "DescriptorTable(CBV(b0), SRV(t0))," \
+                         "DescriptorTable(Sampler(s0))"
 
+
+Texture2D Tex : register(t0);
+sampler TexSampler : register(s0);
 
 cbuffer CONSTANT_BUF : register(b0)
 {
 	float4 color;
 }
 
-[RootSignature(MyRS1)]
-float4 main() : SV_TARGET
+struct PS_INPUT
 {
-	return color;
+  float4 pos : SV_POSITION;
+  float2 uv : TEXCOORD0;
+};
+
+[RootSignature(MyRS1)]
+float4 main(PS_INPUT In) : SV_TARGET
+{
+	return Tex.Sample(TexSampler, In.uv);
 }
--- examples/vtx.hlsl
+++ examples/vtx.hlsl
@@ -1,9 +1,20 @@
 #define MyRS1 "RootFlags( ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT), " \
-                         "DescriptorTable(CBV(b0)) "
+                         "DescriptorTable(CBV(b0), SRV(t0))," \
+                         "DescriptorTable(Sampler(s0))"
+
+
+struct PS_INPUT
+{
+  float4 pos : SV_POSITION;
+  float2 uv : TEXCOORD0;
+};
 
 
 [RootSignature(MyRS1)]
-float4 main(float4 pos : POSITION) : SV_POSITION
+PS_INPUT main(float4 pos : POSITION, float2 texc : TEXCOORD0)
 {
-	return float4(pos.x, pos.y, .5, 1.);
+  PS_INPUT result;
+  result.pos = float4(pos.x, pos.y, .5, 1.);
+  result.uv = texc;
+  return result;
 }
