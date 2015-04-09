a973f8d0373922ff044ad7b1c9fc0e75518cebc2
vlj <vljn.ovi@gmail.com>
2015-04-09 23:33:50 +0200

    YAGF/B3D: Add some more code for skinning

--- include/Core/ISkinnedMesh.h
+++ include/Core/ISkinnedMesh.h
@@ -263,75 +263,22 @@ namespace irr
     class ISkinnedMesh
     {
     public:
-      //! Gets joint count.
-      /** \return Amount of joints in the skeletal animated mesh. */
-      //virtual unsigned getJointCount() const = 0;
 
-      //! Gets the name of a joint.
-      /** \param number: Zero based index of joint. The last joint
-      has the number getJointCount()-1;
-      \return Name of joint and null if an error happened. */
-      //virtual const char* getJointName(unsigned number) const = 0;
-
-            //! Gets a joint number from its name
-            /** \param name: Name of the joint.
-            \return Number of the joint or -1 if not found. */
-            //virtual int getJointNumber(const char* name) const = 0;
-
-                  //! Use animation from another mesh
-                  /** The animation is linked (not copied) based on joint names
-                  so make sure they are unique.
-                  \return True if all joints in this mesh were
-                  matched up (empty names will not be matched, and it's case
-                  sensitive). Unmatched joints will not be animated. */
-                  //virtual bool useAnimationFrom(const ISkinnedMesh *mesh) = 0;
-
-      //! Gets the frame count of the animated mesh.
-      /** \param fps Frames per second to play the animation with. If the amount is 0, it is not animated.
-      The actual speed is set in the scene node the mesh is instantiated in.*/
-      void setAnimationSpeed(float fps)
-      {
-        FramesPerSecond = fps;
-      }
-
-      //! Update Normals when Animating
-      /** \param on If false don't animate, which is faster.
-      Else update normals, which allows for proper lighting of
-      animated meshes. */
-      //      virtual void updateNormalsWhenAnimating(bool on) = 0;
-
-            //! Sets Interpolation Mode
-      //      virtual void setInterpolationMode(E_INTERPOLATION_MODE mode) = 0;
-
-            //! Animates this mesh's joints based on frame input
-      //      virtual void animateMesh(float frame, float blend) = 0;
-
-            //! Preforms a software skin on this mesh based of joint positions
-      //      virtual void skinMesh(float strength = 1.f) = 0;
-
-            //! converts the vertex type of all meshbuffers to tangents.
-            /** E.g. used for bump mapping. */
-      //      virtual void convertMeshToTangents() = 0;
-
-            //! Allows to enable hardware skinning.
-            /* This feature is not implementated in Irrlicht yet */
-      //      virtual bool setHardwareSkinning(bool on) = 0;
-
-            //! A vertex weight
+      //! A vertex weight
       struct SWeight
       {
         //! Index of the mesh buffer
         unsigned short buffer_id; //I doubt 32bits is needed
 
-                       //! Index of the vertex
+                                  //! Index of the vertex
         unsigned short vertex_id; //Store global ID here
 
-                       //! Weight Strength/Percentage (0-1)
+                                  //! Weight Strength/Percentage (0-1)
         float strength;
 
       private:
         //! Internal members used by CSkinnedMesh
-        friend class CSkinnedMesh;
+        friend class ISkinnedMesh;
         bool *Moved;
         core::vector3df StaticPos;
         core::vector3df StaticNormal;
@@ -402,6 +349,7 @@ namespace irr
         core::matrix4 GlobalInversedMatrix; //the x format pre-calculates this
 
       private:
+        friend class ISkinnedMesh;
         SJoint *UseAnimationFrom;
         bool GlobalSkinningSpace;
 
@@ -413,7 +361,392 @@ namespace irr
       std::vector<std::pair<SMeshBufferLightMap, video::SMaterial> > LocalBuffers;
       std::vector<SJoint *> AllJoints;
       float FramesPerSecond;
+      bool HasAnimation;
+      bool PreparedForSkinning;
+      bool AnimateNormals;
+      bool HardwareSkinning;
+      float LastAnimatedFrame;
+      bool SkinnedLastFrame;
+
+      void getFrameData(float frame, SJoint *joint,
+        core::vector3df &position, int &positionHint,
+        core::vector3df &scale, int &scaleHint,
+        core::quaternion &rotation, int &rotationHint)
+      {
+        int foundPositionIndex = -1;
+        int foundScaleIndex = -1;
+        int foundRotationIndex = -1;
+
+        if (joint->UseAnimationFrom)
+        {
+          const std::vector<SPositionKey> &PositionKeys = joint->UseAnimationFrom->PositionKeys;
+          const std::vector<SScaleKey> &ScaleKeys = joint->UseAnimationFrom->ScaleKeys;
+          const std::vector<SRotationKey> &RotationKeys = joint->UseAnimationFrom->RotationKeys;
+
+          if (PositionKeys.size())
+          {
+            foundPositionIndex = -1;
+
+            //Test the Hints...
+            if (positionHint >= 0 && (unsigned)positionHint < PositionKeys.size())
+            {
+              //check this hint
+              if (positionHint > 0 && PositionKeys[positionHint].frame >= frame && PositionKeys[positionHint - 1].frame < frame)
+                foundPositionIndex = positionHint;
+              else if (positionHint + 1 < (int)PositionKeys.size())
+              {
+                //check the next index
+                if (PositionKeys[positionHint + 1].frame >= frame &&
+                  PositionKeys[positionHint + 0].frame < frame)
+                {
+                  positionHint++;
+                  foundPositionIndex = positionHint;
+                }
+              }
+            }
+
+            //The hint test failed, do a full scan...
+            if (foundPositionIndex == -1)
+            {
+              for (unsigned i = 0; i < PositionKeys.size(); ++i)
+              {
+                if (PositionKeys[i].frame >= frame) //Keys should to be sorted by frame
+                {
+                  foundPositionIndex = i;
+                  positionHint = i;
+                  break;
+                }
+              }
+            }
+
+            //Do interpolation...
+            if (foundPositionIndex != -1)
+            {
+              if (//InterpolationMode == EIM_CONSTANT ||
+                false || foundPositionIndex == 0)
+              {
+                position = PositionKeys[foundPositionIndex].position;
+              }
+              else if (true) //(InterpolationMode == EIM_LINEAR)
+              {
+                const SPositionKey& KeyA = PositionKeys[foundPositionIndex];
+                const SPositionKey& KeyB = PositionKeys[foundPositionIndex - 1];
+
+                const float fd1 = frame - KeyA.frame;
+                const float fd2 = KeyB.frame - frame;
+                position = ((KeyB.position - KeyA.position) / (fd1 + fd2))*fd1 + KeyA.position;
+              }
+            }
+          }
+
+          //------------------------------------------------------------
+
+          if (ScaleKeys.size())
+          {
+            foundScaleIndex = -1;
+
+            //Test the Hints...
+            if (scaleHint >= 0 && (unsigned)scaleHint < ScaleKeys.size())
+            {
+              //check this hint
+              if (scaleHint > 0 && ScaleKeys[scaleHint].frame >= frame && ScaleKeys[scaleHint - 1].frame < frame)
+                foundScaleIndex = scaleHint;
+              else if (scaleHint + 1 < (int)ScaleKeys.size())
+              {
+                //check the next index
+                if (ScaleKeys[scaleHint + 1].frame >= frame &&
+                  ScaleKeys[scaleHint + 0].frame < frame)
+                {
+                  scaleHint++;
+                  foundScaleIndex = scaleHint;
+                }
+              }
+            }
+
+            //The hint test failed, do a full scan...
+            if (foundScaleIndex == -1)
+            {
+              for (unsigned i = 0; i < ScaleKeys.size(); ++i)
+              {
+                if (ScaleKeys[i].frame >= frame) //Keys should to be sorted by frame
+                {
+                  foundScaleIndex = i;
+                  scaleHint = i;
+                  break;
+                }
+              }
+            }
+
+            //Do interpolation...
+            if (foundScaleIndex != -1)
+            {
+              if (//InterpolationMode == EIM_CONSTANT ||
+                false || foundScaleIndex == 0)
+              {
+                scale = ScaleKeys[foundScaleIndex].scale;
+              }
+              else if (true)//(InterpolationMode == EIM_LINEAR)
+              {
+                const SScaleKey& KeyA = ScaleKeys[foundScaleIndex];
+                const SScaleKey& KeyB = ScaleKeys[foundScaleIndex - 1];
+
+                const float fd1 = frame - KeyA.frame;
+                const float fd2 = KeyB.frame - frame;
+                scale = ((KeyB.scale - KeyA.scale) / (fd1 + fd2))*fd1 + KeyA.scale;
+              }
+            }
+          }
+
+          //-------------------------------------------------------------
+
+          if (RotationKeys.size())
+          {
+            foundRotationIndex = -1;
+
+            //Test the Hints...
+            if (rotationHint >= 0 && (unsigned)rotationHint < RotationKeys.size())
+            {
+              //check this hint
+              if (rotationHint > 0 && RotationKeys[rotationHint].frame >= frame && RotationKeys[rotationHint - 1].frame < frame)
+                foundRotationIndex = rotationHint;
+              else if (rotationHint + 1 < (int)RotationKeys.size())
+              {
+                //check the next index
+                if (RotationKeys[rotationHint + 1].frame >= frame &&
+                  RotationKeys[rotationHint + 0].frame < frame)
+                {
+                  rotationHint++;
+                  foundRotationIndex = rotationHint;
+                }
+              }
+            }
+
+
+            //The hint test failed, do a full scan...
+            if (foundRotationIndex == -1)
+            {
+              for (unsigned i = 0; i < RotationKeys.size(); ++i)
+              {
+                if (RotationKeys[i].frame >= frame) //Keys should be sorted by frame
+                {
+                  foundRotationIndex = i;
+                  rotationHint = i;
+                  break;
+                }
+              }
+            }
+
+            //Do interpolation...
+            if (foundRotationIndex != -1)
+            {
+              if (//InterpolationMode == EIM_CONSTANT ||
+                false || foundRotationIndex == 0)
+              {
+                rotation = RotationKeys[foundRotationIndex].rotation;
+              }
+              else if (true)//(InterpolationMode == EIM_LINEAR)
+              {
+                const SRotationKey& KeyA = RotationKeys[foundRotationIndex];
+                const SRotationKey& KeyB = RotationKeys[foundRotationIndex - 1];
+
+                const float fd1 = frame - KeyA.frame;
+                const float fd2 = KeyB.frame - frame;
+                const float t = fd1 / (fd1 + fd2);
+
+                /*
+                f32 t = 0;
+                if (KeyA.frame!=KeyB.frame)
+                t = (frame-KeyA.frame) / (KeyB.frame - KeyA.frame);
+                */
+
+                rotation.slerp(KeyA.rotation, KeyB.rotation, t);
+              }
+            }
+          }
+        }
+      }
+
+      void buildAllLocalAnimatedMatrices()
+      {
+        for (SJoint *joint : AllJoints)
+        {
+          //Could be faster:
+          if (joint->UseAnimationFrom &&
+            (joint->UseAnimationFrom->PositionKeys.size() ||
+            joint->UseAnimationFrom->ScaleKeys.size() ||
+            joint->UseAnimationFrom->RotationKeys.size()))
+          {
+            joint->GlobalSkinningSpace = false;
+
+            // IRR_TEST_BROKEN_QUATERNION_USE: TODO - switched to getMatrix_transposed instead of getMatrix for downward compatibility. 
+            //								   Not tested so far if this was correct or wrong before quaternion fix!
+            joint->Animatedrotation.getMatrix_transposed(joint->LocalAnimatedMatrix);
+
+            // --- joint->LocalAnimatedMatrix *= joint->Animatedrotation.getMatrix() ---
+            float *m1 = joint->LocalAnimatedMatrix.pointer();
+            core::vector3df &Pos = joint->Animatedposition;
+            m1[0] += Pos.X * m1[3];
+            m1[1] += Pos.Y * m1[3];
+            m1[2] += Pos.Z * m1[3];
+            m1[4] += Pos.X * m1[7];
+            m1[5] += Pos.Y * m1[7];
+            m1[6] += Pos.Z * m1[7];
+            m1[8] += Pos.X * m1[11];
+            m1[9] += Pos.Y * m1[11];
+            m1[10] += Pos.Z * m1[11];
+            m1[12] += Pos.X * m1[15];
+            m1[13] += Pos.Y * m1[15];
+            m1[14] += Pos.Z * m1[15];
+            // -----------------------------------
+
+            if (joint->ScaleKeys.size())
+            {
+              /*
+              core::matrix4 scaleMatrix;
+              scaleMatrix.setScale(joint->Animatedscale);
+              joint->LocalAnimatedMatrix *= scaleMatrix;
+              */
+
+              // -------- joint->LocalAnimatedMatrix *= scaleMatrix -----------------
+              core::matrix4& mat = joint->LocalAnimatedMatrix;
+              mat[0] *= joint->Animatedscale.X;
+              mat[1] *= joint->Animatedscale.X;
+              mat[2] *= joint->Animatedscale.X;
+              mat[3] *= joint->Animatedscale.X;
+              mat[4] *= joint->Animatedscale.Y;
+              mat[5] *= joint->Animatedscale.Y;
+              mat[6] *= joint->Animatedscale.Y;
+              mat[7] *= joint->Animatedscale.Y;
+              mat[8] *= joint->Animatedscale.Z;
+              mat[9] *= joint->Animatedscale.Z;
+              mat[10] *= joint->Animatedscale.Z;
+              mat[11] *= joint->Animatedscale.Z;
+              // -----------------------------------
+            }
+          }
+          else
+            joint->LocalAnimatedMatrix = joint->LocalMatrix;
+        }
+        SkinnedLastFrame = false;
+      }
+
     public:
+      //! Gets joint count.
+      /** \return Amount of joints in the skeletal animated mesh. */
+      //virtual unsigned getJointCount() const = 0;
+
+      //! Gets the name of a joint.
+      /** \param number: Zero based index of joint. The last joint
+      has the number getJointCount()-1;
+      \return Name of joint and null if an error happened. */
+      //virtual const char* getJointName(unsigned number) const = 0;
+
+            //! Gets a joint number from its name
+            /** \param name: Name of the joint.
+            \return Number of the joint or -1 if not found. */
+            //virtual int getJointNumber(const char* name) const = 0;
+
+                  //! Use animation from another mesh
+                  /** The animation is linked (not copied) based on joint names
+                  so make sure they are unique.
+                  \return True if all joints in this mesh were
+                  matched up (empty names will not be matched, and it's case
+                  sensitive). Unmatched joints will not be animated. */
+                  //virtual bool useAnimationFrom(const ISkinnedMesh *mesh) = 0;
+
+      //! Gets the frame count of the animated mesh.
+      /** \param fps Frames per second to play the animation with. If the amount is 0, it is not animated.
+      The actual speed is set in the scene node the mesh is instantiated in.*/
+      void setAnimationSpeed(float fps)
+      {
+        FramesPerSecond = fps;
+      }
+
+      //! Update Normals when Animating
+      /** \param on If false don't animate, which is faster.
+      Else update normals, which allows for proper lighting of
+      animated meshes. */
+      //      virtual void updateNormalsWhenAnimating(bool on) = 0;
+
+            //! Sets Interpolation Mode
+      //      virtual void setInterpolationMode(E_INTERPOLATION_MODE mode) = 0;
+
+      //! Animates this mesh's joints based on frame input
+      //! blend: {0-old position, 1-New position}
+      void animateMesh(float frame, float blend)
+      {
+        if (!HasAnimation || LastAnimatedFrame == frame)
+          return;
+
+        LastAnimatedFrame = frame;
+        SkinnedLastFrame = false;
+
+        if (blend <= 0.f)
+          return; //No need to animate
+
+        for (unsigned i = 0; i < AllJoints.size(); ++i)
+        {
+          //The joints can be animated here with no input from their
+          //parents, but for setAnimationMode extra checks are needed
+          //to their parents
+          SJoint *joint = AllJoints[i];
+
+          const core::vector3df oldPosition = joint->Animatedposition;
+          const core::vector3df oldScale = joint->Animatedscale;
+          const core::quaternion oldRotation = joint->Animatedrotation;
+
+          core::vector3df position = oldPosition;
+          core::vector3df scale = oldScale;
+          core::quaternion rotation = oldRotation;
+
+          getFrameData(frame, joint,
+            position, joint->positionHint,
+            scale, joint->scaleHint,
+            rotation, joint->rotationHint);
+
+          if (blend == 1.0f)
+          {
+            //No blending needed
+            joint->Animatedposition = position;
+            joint->Animatedscale = scale;
+            joint->Animatedrotation = rotation;
+          }
+          else
+          {
+            //Blend animation
+            joint->Animatedposition = (1.f - blend) * oldPosition + blend * position;
+            joint->Animatedscale = (1.f - blend) * oldScale + blend * scale;
+            joint->Animatedrotation.slerp(oldRotation, rotation, blend);
+          }
+        }
+
+        //Note:
+        //LocalAnimatedMatrix needs to be built at some point, but this function may be called lots of times for
+        //one render (to play two animations at the same time) LocalAnimatedMatrix only needs to be built once.
+        //a call to buildAllLocalAnimatedMatrices is needed before skinning the mesh, and before the user gets the joints to move
+
+        //----------------
+        // Temp!
+        buildAllLocalAnimatedMatrices();
+      }
+
+      //! Preforms a software skin on this mesh based of joint positions
+//      virtual void skinMesh(float strength = 1.f) = 0;
+
+      //! converts the vertex type of all meshbuffers to tangents.
+      /** E.g. used for bump mapping. */
+//      virtual void convertMeshToTangents() = 0;
+
+      //! Allows to enable hardware skinning.
+      /* This feature is not implementated in Irrlicht yet */
+//      virtual bool setHardwareSkinning(bool on) = 0;
+
+    public:
+
+      ISkinnedMesh() : HasAnimation(false)
+      {
+
+      }
 
       ~ISkinnedMesh()
       {
