7ba48689a3741ebf170bb784993b42018a005a3e
vlj <vljn.ovi@gmail.com>
2015-04-13 22:06:43 +0200

    YAGF/scene: Use gbuffer shader

--- examples/Scene/include/Material.h
+++ examples/Scene/include/Material.h
@@ -64,15 +64,37 @@ void main()
 }
 );
 
-const char *fragshader =
-"#version 330\n"
-"uniform sampler2D tex;\n"
-"in vec2 uv;\n"
-"out vec4 FragColor;\n"
-"void main() {\n"
-"  FragColor = texture(tex, uv);\n"
-"}\n";
+const char *fragshader = TO_STRING(
+  \#version 330 \n
 
+uniform sampler2D tex;
+uniform sampler2D glosstex;
+
+in vec3 nor;
+in vec2 uv;
+in vec4 color;
+
+layout(location = 0) out vec4 EncodedNormal_Roughness_Metalness;
+layout(location = 1) out vec4 Colors;
+layout(location = 2) out float EmitMap;
+
+// from Crytek "a bit more deferred CryEngine"
+vec2 EncodeNormal(vec3 n)
+{
+  return normalize(n.xy) * sqrt(n.z * 0.5 + 0.5);
+}
+
+void main(void)
+{
+  Colors = vec4(texture(tex, uv).rgb * pow(color.rgb, vec3(2.2)), 1.);
+  float glossmap = texture(glosstex, uv).r;
+  float reflectance = texture(glosstex, uv).g;
+  EncodedNormal_Roughness_Metalness.xy = 0.5 * EncodeNormal(normalize(nor)) + 0.5;
+  EncodedNormal_Roughness_Metalness.z = 1. - glossmap;
+  EncodedNormal_Roughness_Metalness.w = reflectance;
+  EmitMap = texture(glosstex, uv).b;
+}
+);
 
 class ObjectShader : public ShaderHelperSingleton<ObjectShader>, public TextureRead<UniformBufferResource<0>, UniformBufferResource<1>, TextureResource<GL_TEXTURE_2D, 0> >
 {
