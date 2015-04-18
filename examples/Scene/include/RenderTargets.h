192526e49379b6b3031e5abe19b947bb01ac6a6c
vlj <vljn.ovi@gmail.com>
2015-04-19 00:39:13 +0200

    YAGF/scene: Move RenderTargets.h code to .cpp

--- examples/Scene/include/RenderTargets.h
+++ examples/Scene/include/RenderTargets.h
@@ -32,37 +32,11 @@ private:
   WrapperResource* RenderTargetTextures[RTT_COUNT];
   WrapperRTTSet* RTTSets[FBO_COUNT];
   WrapperResource *depthbuffer;
-
 public:
-  RenderTargets(size_t w, size_t h): Width(w), Height(h)
-  {
-    depthbuffer = GlobalGFXAPI->createDepthStencilTexture(Width, Height);
-
-    float color[] = { 0., 0., 0., 0. };
-    RenderTargetTextures[GBUFFER_NORMAL_AND_DEPTH] = GlobalGFXAPI->createRTT(irr::video::ECF_R16G16B16A16F, Width, Height, color);
-    RenderTargetTextures[GBUFFER_BASE_COLOR] = GlobalGFXAPI->createRTT(irr::video::ECF_R8G8B8A8_UNORM_SRGB, Width, Height, color);
-    RenderTargetTextures[COLORS] = GlobalGFXAPI->createRTT(irr::video::ECF_R16G16B16A16F, Width, Height, color);
-
-    RTTSets[FBO_GBUFFER] = GlobalGFXAPI->createRTTSet(
-    { RenderTargetTextures[GBUFFER_NORMAL_AND_DEPTH] , RenderTargetTextures[GBUFFER_BASE_COLOR] },
-    { irr::video::ECF_R16G16B16A16F, irr::video::ECF_R8G8B8A8_UNORM_SRGB },
-    Width, Height,
-    depthbuffer);
-  }
-
-  ~RenderTargets()
-  {
-  }
-
-  WrapperResource* getRTT(enum RTT tp)
-  {
-    return RenderTargetTextures[tp];
-  }
-
-  WrapperRTTSet* getRTTSet(enum RTTSet tp)
-  {
-    return RTTSets[tp];
-  }
+  RenderTargets(size_t w, size_t h);
+  ~RenderTargets();
+  WrapperResource* getRTT(enum RTT tp);
+  WrapperRTTSet* getRTTSet(enum RTTSet tp);
 
 };
 
---
+++ examples/Scene/rendertargets.cpp
@@ -0,0 +1,34 @@
+// Copyright (C) 2015 Vincent Lejeune
+// For conditions of distribution and use, see copyright notice in License.txt
+#include <RenderTargets.h>
+
+
+RenderTargets::RenderTargets(size_t w, size_t h) : Width(w), Height(h)
+{
+  depthbuffer = GlobalGFXAPI->createDepthStencilTexture(Width, Height);
+
+  float color[] = { 0., 0., 0., 0. };
+  RenderTargetTextures[GBUFFER_NORMAL_AND_DEPTH] = GlobalGFXAPI->createRTT(irr::video::ECF_R16G16B16A16F, Width, Height, color);
+  RenderTargetTextures[GBUFFER_BASE_COLOR] = GlobalGFXAPI->createRTT(irr::video::ECF_R8G8B8A8_UNORM_SRGB, Width, Height, color);
+  RenderTargetTextures[COLORS] = GlobalGFXAPI->createRTT(irr::video::ECF_R16G16B16A16F, Width, Height, color);
+
+  RTTSets[FBO_GBUFFER] = GlobalGFXAPI->createRTTSet(
+  { RenderTargetTextures[GBUFFER_NORMAL_AND_DEPTH] , RenderTargetTextures[GBUFFER_BASE_COLOR] },
+  { irr::video::ECF_R16G16B16A16F, irr::video::ECF_R8G8B8A8_UNORM_SRGB },
+  Width, Height,
+  depthbuffer);
+}
+
+RenderTargets::~RenderTargets()
+{
+}
+
+WrapperResource* RenderTargets::getRTT(enum RTT tp)
+{
+  return RenderTargetTextures[tp];
+}
+
+WrapperRTTSet* RenderTargets::getRTTSet(enum RTTSet tp)
+{
+  return RTTSets[tp];
+}
