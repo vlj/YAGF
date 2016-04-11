b47fece5300c14068c7e7662c0faddc03a3e272b
Vincent Lejeune <vljn.ovi@gmail.com>
2016-04-12 00:47:57 +0200

    Reenable clearing in meshdx12.

--- examples/meshdx12/meshdx12.cpp
+++ examples/meshdx12/meshdx12.cpp
@@ -210,6 +210,7 @@ namespace
         heapdesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
         heapdesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
         CHECK_HRESULT(dev->CreateDescriptorHeap(&heapdesc, IID_PPV_ARGS(result.GetAddressOf())));
+        return result;
     }
 
     void create_constant_buffer_view(device_t dev, descriptor_storage_t storage, size_t index, buffer_t buffer, uint32_t buffer_size)
@@ -273,6 +274,113 @@ namespace
         dev->CreateShaderResourceView(img.Get(), &desc, CD3DX12_CPU_DESCRIPTOR_HANDLE(storage->GetCPUDescriptorHandleForHeapStart()).Offset(index, stride));
     }
 
+    void start_command_list_recording(device_t dev, command_list_t command_list, command_list_storage_t storage)
+    {
+        command_list->Reset(storage.Get(), nullptr);
+    }
+
+    void make_command_list_executable(device_t dev, command_list_t command_list)
+    {
+        command_list->Close();
+    }
+
+    void wait_for_command_queue_idle(device_t dev, command_queue_t command_queue)
+    {
+        Microsoft::WRL::ComPtr<ID3D12Fence> fence;
+        CHECK_HRESULT(dev->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(fence.GetAddressOf())));
+        HANDLE completion_event = CreateEvent(0, FALSE, FALSE, 0);
+        fence->SetEventOnCompletion(1, completion_event);
+        command_queue->Signal(fence.Get(), 1);
+        WaitForSingleObject(completion_event, INFINITE);
+        CloseHandle(completion_event);
+    }
+
+    struct framebuffer_t
+    {
+        Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> rtt_heap;
+        Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> dsv_heap;
+        size_t NumRTT;
+        bool hasDepthStencil;
+
+        framebuffer_t() {}
+
+        framebuffer_t(device_t dev, const std::vector<ID3D12Resource*> &RTTs, ID3D12Resource *DepthStencil)
+            : NumRTT(RTTs.size()), hasDepthStencil(DepthStencil != nullptr)
+        {
+            D3D12_DESCRIPTOR_HEAP_DESC rtt_desc = {};
+            rtt_desc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+            rtt_desc.NumDescriptors = NumRTT;
+            CHECK_HRESULT(dev->CreateDescriptorHeap(&rtt_desc, IID_PPV_ARGS(rtt_heap.GetAddressOf())));
+
+            for (unsigned i = 0; i < RTTs.size(); i++)
+            {
+                D3D12_RENDER_TARGET_VIEW_DESC rttvd = {};
+                rttvd.Format = RTTs[i]->GetDesc().Format;
+                rttvd.ViewDimension = D3D12_RTV_DIMENSION_TEXTURE2D;
+                rttvd.Texture2D.MipSlice = 0;
+                rttvd.Texture2D.PlaneSlice = 0;
+                dev->CreateRenderTargetView(RTTs[i], &rttvd, CD3DX12_CPU_DESCRIPTOR_HANDLE(rtt_heap->GetCPUDescriptorHandleForHeapStart()).Offset(i, dev->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV)));
+            }
+
+            if (DepthStencil == nullptr)
+                return;
+
+            D3D12_DESCRIPTOR_HEAP_DESC dsv_desc = {};
+            dsv_desc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_DSV;
+            dsv_desc.NumDescriptors = 1;
+            CHECK_HRESULT(dev->CreateDescriptorHeap(&dsv_desc, IID_PPV_ARGS(dsv_heap.GetAddressOf())));
+            D3D12_DEPTH_STENCIL_VIEW_DESC DSVDescription = {};
+            DSVDescription.Format = DepthStencil->GetDesc().Format;
+            DSVDescription.ViewDimension = D3D12_DSV_DIMENSION_TEXTURE2D;
+            DSVDescription.Texture2D.MipSlice = 0;
+            dev->CreateDepthStencilView(DepthStencil, &DSVDescription, dsv_heap->GetCPUDescriptorHandleForHeapStart());
+        }
+
+        ~framebuffer_t()
+        {
+
+        }
+
+/*        void Bind(ID3D12GraphicsCommandList *cmdlist)
+        {
+            D3D12_RECT rect = {};
+            rect.left = 0;
+            rect.top = 0;
+            rect.bottom = (LONG)width;
+            rect.right = (LONG)height;
+
+            D3D12_VIEWPORT view = {};
+            view.Height = (FLOAT)width;
+            view.Width = (FLOAT)height;
+            view.TopLeftX = 0;
+            view.TopLeftY = 0;
+            view.MinDepth = 0;
+            view.MaxDepth = 1.;
+
+            cmdlist->RSSetViewports(1, &view);
+            cmdlist->RSSetScissorRects(1, &rect);
+
+            if (hasDepthStencil)
+                cmdlist->SetRenderTargets(&DescriptorHeap->GetCPUDescriptorHandleForHeapStart(), true, (UINT)NumRTT, &DepthDescriptorHeap->GetCPUDescriptorHandleForHeapStart());
+            else
+                cmdlist->SetRenderTargets(&DescriptorHeap->GetCPUDescriptorHandleForHeapStart(), true, (UINT)NumRTT, nullptr);
+        }
+
+        void Clear(ID3D12GraphicsCommandList *cmdlist, float clearColor[4])
+        {
+            UINT Increment = Context::getInstance()->dev->GetDescriptorHandleIncrementSize(D3D12_RTV_DESCRIPTOR_HEAP);
+            for (unsigned i = 0; i < NumRTT; i++)
+            {
+                cmdlist->ClearRenderTargetView(DescriptorHeap->GetCPUDescriptorHandleForHeapStart().MakeOffsetted(i * Increment), clearColor, 0, 0);
+            }
+        }
+
+        void ClearDepthStencil(ID3D12GraphicsCommandList *cmdlist, float Depth, unsigned Stencil)
+        {
+            cmdlist->ClearDepthStencilView(DepthDescriptorHeap->GetCPUDescriptorHandleForHeapStart(), D3D12_CLEAR_DEPTH, Depth, Stencil, nullptr, 0);
+        }*/
+    };
+
 /*    {
     D3D12_DEPTH_STENCIL_VIEW_DESC dsv = {};
     dsv.Format = DXGI_FORMAT_D32_FLOAT;
@@ -322,7 +430,7 @@ struct Sample
     std::unordered_map<std::string, uint32_t> textureSet;
 
     pipeline_state_t objectpso;
-//    D3DRTTSet *fbo[2];
+    framebuffer_t fbo[2];
 
     irr::scene::CB3DMeshFileLoader *loader;
 
@@ -343,8 +451,8 @@ struct Sample
 
         depth_buffer = create_image(dev, DXGI_FORMAT_D24_UNORM_S8_UINT, 1024, 1024, 1, D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL);
 
-//        fbo[0] = new D3DRTTSet({ Context::getInstance()->getBackBuffer(0) }, { DXGI_FORMAT_R8G8B8A8_UNORM_SRGB }, 1024, 1024, depth_buffer->D3DValue.resource, &depth_buffer->D3DValue.description.TextureView.DSV);
-//        fbo[1] = new D3DRTTSet({ Context::getInstance()->getBackBuffer(1) }, { DXGI_FORMAT_R8G8B8A8_UNORM_SRGB }, 1024, 1024, depth_buffer->D3DValue.resource, &depth_buffer->D3DValue.description.TextureView.DSV);
+        fbo[0] = framebuffer_t(dev, { back_buffer[0].Get() }, depth_buffer.Get());
+        fbo[1] = framebuffer_t(dev, { back_buffer[1].Get() }, depth_buffer.Get());
 
         irr::io::CReadFile reader("..\\examples\\assets\\xue.b3d");
         loader = new irr::scene::CB3DMeshFileLoader(&reader);
@@ -502,12 +610,25 @@ struct Sample
             create_image_view(dev, TexResourceHeaps, texture_id++, texture);
         }
 //        objectpso = createSkinnedObjectShader();
+        make_command_list_executable(dev, command_list);
+        cmdqueue->ExecuteCommandLists(1, (ID3D12CommandList**)command_list.GetAddressOf());
     }
 
 public:
     Sample(HWND window)
     {
-        CHECK_HRESULT(D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(dev.GetAddressOf())));
+#ifndef NDEBUG
+        Microsoft::WRL::ComPtr<ID3D12Debug> debugInterface;
+        D3D12GetDebugInterface(IID_PPV_ARGS(&debugInterface));
+        debugInterface->EnableDebugLayer();
+#endif //  DEBUG
+
+        Microsoft::WRL::ComPtr<IDXGIFactory> fact;
+        CHECK_HRESULT(CreateDXGIFactory(IID_PPV_ARGS(fact.GetAddressOf())));
+        Microsoft::WRL::ComPtr<IDXGIAdapter> adaptater;
+        CHECK_HRESULT(fact->EnumAdapters(0, adaptater.GetAddressOf()));
+
+        CHECK_HRESULT(D3D12CreateDevice(adaptater.Get(), D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(dev.GetAddressOf())));
 
         D3D12_COMMAND_QUEUE_DESC cmddesc = { D3D12_COMMAND_LIST_TYPE_DIRECT };
         CHECK_HRESULT(dev->CreateCommandQueue(&cmddesc, IID_PPV_ARGS(&cmdqueue)));
@@ -522,8 +643,7 @@ public:
         swapChain.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
         swapChain.SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL;
 
-        Microsoft::WRL::ComPtr<IDXGIFactory> fact;
-        CHECK_HRESULT(CreateDXGIFactory(IID_PPV_ARGS(fact.GetAddressOf())));
+
         CHECK_HRESULT(fact->CreateSwapChain(cmdqueue.Get(), &swapChain, (IDXGISwapChain**)chain.GetAddressOf()));
         CHECK_HRESULT(chain->GetBuffer(0, IID_PPV_ARGS(&back_buffer[0])));
         CHECK_HRESULT(chain->GetBuffer(1, IID_PPV_ARGS(&back_buffer[1])));
@@ -536,12 +656,12 @@ public:
 
     ~Sample()
     {
-
+        wait_for_command_queue_idle(dev, cmdqueue);
     }
 
     void Draw()
     {
-//        GlobalGFXAPI->openCommandList(cmdlist);
+        start_command_list_recording(dev, command_list, command_allocator);
         Matrixes *cbufdata = static_cast<Matrixes*>(map_buffer(dev, cbuffer));
         irr::core::matrix4 Model, View;
         View.buildProjectionMatrixPerspectiveFovLH(70.f / 180.f * 3.14f, 1.f, 1.f, 100.f);
@@ -566,11 +686,12 @@ public:
         command_list->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(back_buffer[chain->GetCurrentBackBufferIndex()].Get(), D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET));
 
         float clearColor[] = { .25f, .25f, 0.35f, 1.0f };
-//        fbo[Context::getInstance()->getCurrentBackBufferIndex()]->Clear(cmdlist->D3DValue.CommandList, clearColor);
+        command_list->ClearRenderTargetView(fbo[chain->GetCurrentBackBufferIndex()].rtt_heap->GetCPUDescriptorHandleForHeapStart(), clearColor, 0, nullptr);
+//        fbo[chain->GetCurrentBackBufferIndex()]->Clear(cmdlist->D3DValue.CommandList, clearColor);
 //        fbo[Context::getInstance()->getCurrentBackBufferIndex()]->ClearDepthStencil(cmdlist->D3DValue.CommandList, 1.f, 0);
 
 //        fbo[Context::getInstance()->getCurrentBackBufferIndex()]->Bind(cmdlist->D3DValue.CommandList);
-        command_list->SetPipelineState(objectpso.Get());
+//        command_list->SetPipelineState(objectpso.Get());
 
         std::array<ID3D12DescriptorHeap*, 2> descriptors = {constant_buffer_descriptors.Get(), sampler_heap.Get()};
         command_list->SetDescriptorHeaps(2, descriptors.data());
@@ -587,12 +708,10 @@ public:
         }
 
         command_list->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(back_buffer[chain->GetCurrentBackBufferIndex()].Get(), D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT));
-        //GlobalGFXAPI->closeCommandList(cmdlist);
-        //GlobalGFXAPI->submitToQueue(cmdlist);
-//        HANDLE handle = getCPUSyncHandle(Context::getInstance()->cmdqueue.Get());
-//        WaitForSingleObject(handle, INFINITE);
-//        CloseHandle(handle);
-//        Context::getInstance()->Swap();
+        make_command_list_executable(dev, command_list);
+        cmdqueue->ExecuteCommandLists(1, (ID3D12CommandList**)command_list.GetAddressOf());
+        wait_for_command_queue_idle(dev, cmdqueue);
+        CHECK_HRESULT(chain->Present(1, 0));
     }
 };
 
