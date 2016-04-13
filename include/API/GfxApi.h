a323f572cf9d5f4260cf8b7f58610573876cd284
Vincent Lejeune <vljn.ovi@gmail.com>
2016-04-13 19:14:10 +0200

    Forget this hunk

--- include/API/GfxApi.h
+++ include/API/GfxApi.h
@@ -44,14 +44,14 @@ command_list_t create_command_list(device_t dev, command_list_storage_t storage)
 buffer_t create_buffer(device_t dev, size_t size);
 void* map_buffer(device_t dev, buffer_t buffer);
 void unmap_buffer(device_t dev, buffer_t buffer);
-image_t create_image(device_t dev, DXGI_FORMAT format, uint32_t width, uint32_t height, uint16_t mipmap, D3D12_RESOURCE_FLAGS flags, D3D12_RESOURCE_STATES initial_state, D3D12_CLEAR_VALUE *clear_value);
+//image_t create_image(device_t dev, DXGI_FORMAT format, uint32_t width, uint32_t height, uint16_t mipmap, D3D12_RESOURCE_FLAGS flags, D3D12_RESOURCE_STATES initial_state, D3D12_CLEAR_VALUE *clear_value);
 descriptor_storage_t create_descriptor_storage(device_t dev, uint32_t num_descriptors);
 void create_constant_buffer_view(device_t dev, descriptor_storage_t storage, uint32_t index, buffer_t buffer, uint32_t buffer_size);
 descriptor_storage_t create_sampler_heap(device_t dev, uint32_t num_descriptors);
 void create_sampler(device_t dev, descriptor_storage_t storage, uint32_t index, SAMPLER_TYPE sampler_type);
 void create_image_view(device_t dev, descriptor_storage_t storage, uint32_t index, image_t img);
 void start_command_list_recording(device_t dev, command_list_t command_list, command_list_storage_t storage);
-void make_command_list_executable(device_t dev, command_list_t command_list);
+void make_command_list_executable(command_list_t command_list);
 void wait_for_command_queue_idle(device_t dev, command_queue_t command_queue);
 void present(device_t dev, swap_chain_t chain);
 void set_pipeline_barrier(device_t dev, command_list_t command_list, image_t resource, RESOURCE_USAGE before, RESOURCE_USAGE after, uint32_t subresource);
