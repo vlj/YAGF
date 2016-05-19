7fff2bc9c0f28c94438d3ae9fae58d3e9951a886
Vincent Lejeune <vljn.ovi@gmail.com>
2016-05-19 16:06:50 +0200

    Use a dummy System Form window.

--- examples/meshdx12/mesh-managed/Class1.cs
+++ examples/meshdx12/mesh-managed/Class1.cs
@@ -1,11 +1,26 @@
 ﻿using System;
+using System.Windows.Forms;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace mesh_managed
 {
+    public class FormTest : Form
+    {
+        public Button button1;
+        public FormTest()
+        {
+            button1 = new Button();
+            button1.Size = new Size(40, 40);
+            button1.Location = new Point(30, 30);
+            button1.Text = "Click me";
+            this.Controls.Add(button1);
+        }
+    }
+
     public sealed class CustomAppDomainManager : AppDomainManager, Interface1
     {
         public CustomAppDomainManager()
@@ -20,6 +35,8 @@ namespace mesh_managed
 
         public string HelloWorld(string name)
         {
+            var tmp = new FormTest();
+            tmp.Show();
             return "Hello " + name;
         }
     }
