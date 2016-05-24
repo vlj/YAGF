119c88701a27fe33cf624a829346ed3b42185d71
Vincent Lejeune <vljn.ovi@gmail.com>
2016-05-25 00:17:10 +0200

    Add tressFX begin/end

--- examples/TressFX/TressFX.vulkan/sample.cpp
+++ examples/TressFX/TressFX.vulkan/sample.cpp
@@ -2,11 +2,17 @@
 #include <Api/Vkapi.h>
 #include "TFXFileIO.h"
 
+static DirectX::XMVECTOR         g_lightEyePt = DirectX::XMVectorSet(-421.25043f, 306.7890949f, -343.22232f, 1.0f);
+
 sample::sample(HINSTANCE hinstance, HWND hwnd)
 {
     auto dev_swapchain_queue = create_device_swapchain_and_graphic_presentable_queue(hinstance, hwnd);
-    tressfx_helper.pvkDevice = *std::get<0>(dev_swapchain_queue);
-    tressfx_helper.texture_memory_index = std::get<0>(dev_swapchain_queue)->default_memory_index;
+    std::unique_ptr<device_t> dev = std::move(std::get<0>(dev_swapchain_queue));
+    tressfx_helper.pvkDevice = *dev;
+    tressfx_helper.texture_memory_index = dev->default_memory_index;
+
+    tressfx_helper.lightPosition = g_lightEyePt;
+
     TressFX_Initialize(tressfx_helper);
 
     TFXProjectFile tfxproject;
@@ -45,5 +51,12 @@ sample::sample(HINSTANCE hinstance, HWND hwnd)
 
         TressFX_LoadRawAsset(tressfx_helper, guideFollowParams, &hairBlob);
     }
-    TressFX_CreateProcessedAsset(tressfx_helper, nullptr, nullptr, nullptr);
+    std::unique_ptr<command_list_storage_t> command_storage = create_command_storage(*dev);
+    std::unique_ptr<command_list_t> upload_command_buffer = create_command_list(*dev, *command_storage);
+    start_command_list_recording(*upload_command_buffer, *command_storage);
+    std::unique_ptr<buffer_t> upload_buffer = create_buffer(*dev, 1024 * 1024 * 128, irr::video::E_MEMORY_POOL::EMP_CPU_WRITEABLE, usage_buffer_transfer_src);
+    TressFX_CreateProcessedAsset(tressfx_helper, nullptr, nullptr, nullptr, 0, *upload_command_buffer, *upload_buffer, *upload_buffer->baking_memory);
+
+    TressFX_Begin(tressfx_helper);
+    TressFX_End(tressfx_helper);
 }
