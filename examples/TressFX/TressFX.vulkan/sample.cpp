70ef1e28480df924c5dbbe21c78821abab696bd6
Vincent Lejeune <vljn.ovi@gmail.com>
2016-05-24 19:29:56 +0200

    Load tfxproj file

--- examples/TressFX/TressFX.vulkan/sample.cpp
+++ examples/TressFX/TressFX.vulkan/sample.cpp
@@ -1,5 +1,6 @@
 #include "sample.h"
 #include <Api/Vkapi.h>
+#include "TFXFileIO.h"
 
 sample::sample(HINSTANCE hinstance, HWND hwnd)
 {
@@ -7,4 +8,42 @@ sample::sample(HINSTANCE hinstance, HWND hwnd)
     tressfx_helper.pvkDevice = *std::get<0>(dev_swapchain_queue);
     tressfx_helper.texture_memory_index = std::get<0>(dev_swapchain_queue)->default_memory_index;
     TressFX_Initialize(tressfx_helper);
+
+    TFXProjectFile tfxproject;
+    bool tmp = tfxproject.Read(L"..\\..\\..\\TressFX\\amd_tressfx_viewer\\media\\testhair1\\TestHair1.tfxproj");
+
+    AMD::TressFX_HairBlob hairBlob;
+
+    const int numHairSectionsTotal = tfxproject.CountTFXFiles();
+    std::ifstream tfxFile;
+
+    for (int i = 0; i < numHairSectionsTotal; ++i)
+    {
+        tfxFile.clear();
+        tfxFile.open(tfxproject.mTFXFile[i].tfxFileName, std::ios::binary | std::ifstream::in);
+
+        if (tfxFile.fail())
+        {
+            break;
+        }
+
+        std::filebuf* pbuf = tfxFile.rdbuf();
+        size_t size = pbuf->pubseekoff(0, tfxFile.end, tfxFile.in);
+        pbuf->pubseekpos(0, tfxFile.in);
+        hairBlob.pHair = (void *) new char[size];
+        tfxFile.read((char *)hairBlob.pHair, size);
+        tfxFile.close();
+        hairBlob.size = (unsigned)size;
+
+        hairBlob.animationSize = 0;
+        hairBlob.pAnimation = NULL;
+
+        AMD::TressFX_GuideFollowParams guideFollowParams;
+        guideFollowParams.numFollowHairsPerGuideHair = tfxproject.mTFXFile[i].numFollowHairsPerGuideHair;
+        guideFollowParams.radiusForFollowHair = tfxproject.mTFXFile[i].radiusForFollowHair;
+        guideFollowParams.tipSeparationFactor = tfxproject.mTFXFile[i].tipSeparationFactor;
+
+        TressFX_LoadRawAsset(tressfx_helper, guideFollowParams, &hairBlob);
+    }
+    TressFX_CreateProcessedAsset(tressfx_helper, nullptr, nullptr, nullptr);
 }
