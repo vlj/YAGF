f80b62936b42ed609c5db25d8887ed2a1bb86237
Vincent Lejeune <vljn.ovi@gmail.com>
2016-05-25 00:49:12 +0200

    Add some extra param.

--- examples/TressFX/TressFX.vulkan/sample.cpp
+++ examples/TressFX/TressFX.vulkan/sample.cpp
@@ -11,6 +11,9 @@ sample::sample(HINSTANCE hinstance, HWND hwnd)
     tressfx_helper.texture_memory_index = dev->default_memory_index;
 
     tressfx_helper.lightPosition = g_lightEyePt;
+    tressfx_helper.bShortCutOn = false;
+    tressfx_helper.hairParams.bAntialias = false;
+    tressfx_helper.hairParams.strandCopies = 1;
 
     TressFX_Initialize(tressfx_helper);
 
@@ -56,6 +59,11 @@ sample::sample(HINSTANCE hinstance, HWND hwnd)
     std::unique_ptr<buffer_t> upload_buffer = create_buffer(*dev, 1024 * 1024 * 128, irr::video::E_MEMORY_POOL::EMP_CPU_WRITEABLE, usage_buffer_transfer_src);
     TressFX_CreateProcessedAsset(tressfx_helper, nullptr, nullptr, nullptr, 0, *upload_command_buffer, *upload_buffer, *upload_buffer->baking_memory);
     std::unique_ptr<buffer_t> constant_buffer = create_buffer(*dev, 1024 * 1024 * 10, irr::video::E_MEMORY_POOL::EMP_CPU_WRITEABLE, usage_buffer_transfer_src);
+
+    std::unique_ptr<command_list_t> draw_command_buffer = create_command_list(*dev, *command_storage);
+    start_command_list_recording(*draw_command_buffer, *command_storage);
+
     TressFX_Begin(tressfx_helper, *constant_buffer, *constant_buffer->baking_memory, 0);
+    TressFX_Render(tressfx_helper, *draw_command_buffer);
     TressFX_End(tressfx_helper);
 }
